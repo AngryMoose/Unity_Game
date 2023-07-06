4598b57e9b511ecaca00ba9b20a0c55485b051ff
AngryMoose <83735249+AngryMoose@users.noreply.github.com>
2023-07-05 22:30:29 -0400

    Jump Pad

---
+++ Space_Pirate_Game V0.0.0.1/Assets/Scripts/jumpPadLaunch.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpPadLaunch : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float launchSpeed;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("On Pad");
+        anim.SetTrigger("startJumpPad");
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        rb.velocity = new Vector2(rb.velocity.x, launchSpeed);
+    }
+
+
+
+}
